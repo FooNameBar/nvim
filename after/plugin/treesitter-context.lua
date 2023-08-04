716f7199149e18785583fcd3ed5e5009eb0e71db
FooNameBar <itssaturdayeverday@gmail.com>
2023-08-04 15:46:33 -0700

    Rebound context shortcut

--- after/plugin/treesitter-context.lua
+++ after/plugin/treesitter-context.lua
@@ -10,11 +10,11 @@ ctx.setup {
   mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
   -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = '_',
+  separator = nil,
   zindex = 20,     -- The Z-index of the context window
   on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
 
 vim.cmd('hi TreesitterContextBottom gui=underline guisp=Grey')
 
-vim.keymap.set("n", "<leader>n", ctx.go_to_context, { silent = true})
+vim.keymap.set("n", "<leader>fc", ctx.go_to_context, { silent = true})
