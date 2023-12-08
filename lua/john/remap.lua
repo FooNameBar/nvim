b04844d7538dc1c16fe0204efc3f22b4059a23be
FooNameBar <itssaturdayeverday@gmail.com>
2023-12-07 17:36:30 -0800

    <leader>pc changed to <leader>P, <leader>d fixed

--- lua/john/remap.lua
+++ lua/john/remap.lua
@@ -18,9 +18,9 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>pc", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+p]])
 
-vim.keymap.set("v", "<leader>d", [["+d]])
+vim.keymap.set("v", "<leader>D", [["+d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -38,4 +38,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>we", "<cmd>w | :Ex<CR>", { silent = true })
-vim.keymap.set("n", "<leader>wq", "<cmd>w | :q<CR>", { silent = true })
