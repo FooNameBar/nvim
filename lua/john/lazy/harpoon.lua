5d0712d627de64ee4e132b0a05e2c5b0f0593c38
FooNameBar <itssaturdayeverday@gmail.com>
2024-01-03 19:03:58 -0800

    Changed harpoon keymaps over to lazy integration because they were not working otherwise

--- lua/john/lazy/harpoon.lua
+++ lua/john/lazy/harpoon.lua
@@ -2,20 +2,64 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = true,
-    config = function()
-        local harpoon = require("harpoon")
-
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        --[[
-        ]]--
-
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-    end,
+    opts = {},
+    keys = {
+        {
+            "<leader>a",
+            function()
+                require("harpoon"):list():append()
+            end,
+            desc = "Add current file to list",
+        },
+        {
+            "<C-e>",
+            function()
+                local h = require("harpoon")
+                h.ui:toggle_quick_menu(h:list())
+            end,
+            desc = "Show list",
+        },
+        {
+            "<C-h>",
+            function()
+                require("harpoon"):list():select(1)
+            end,
+            desc = "Go to first mark",
+        },
+        {
+            "<C-j>",
+            function()
+                require("harpoon"):list():select(2)
+            end,
+            desc = "Go to second mark",
+        },
+        {
+            "<C-k>",
+            function()
+                require("harpoon"):list():select(3)
+            end,
+            desc = "Go to third mark",
+        },
+        {
+            "<C-l>",
+            function()
+                require("harpoon"):list():select(4)
+            end,
+            desc = "Go to fourth mark",
+        },
+        {
+            "<leader>hn",
+            function()
+                require("harpoon"):list():next()
+            end,
+            desc = "Go to next mark",
+        },
+        {
+            "<leader>hp",
+            function()
+                require("harpoon"):list():prev()
+            end,
+            desc = "Go to previous mark",
+        },
+    },
 }
