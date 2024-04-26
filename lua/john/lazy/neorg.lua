cde0487ed7443675486d748527f9b2458cd44e2d
FooNameBar <itssaturdayeverday@gmail.com>
2024-04-25 22:02:26 -0700

    Updated plugins, breaking Neorg change needs luarocks.nvim dependecy now

--- lua/john/lazy/neorg.lua
+++ lua/john/lazy/neorg.lua
@@ -1,32 +1,38 @@
 return {
-    "nvim-neorg/neorg",
-    event = "VeryLazy",
-    build = ":Neorg sync-parsers",
-    dependecies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        require('neorg').setup {
-            load = {
-                ["core.defaults"] = {},
-                ["core.concealer"] = {
-                    config = {
-                        folds = false,
-                    }
-                },
-                ["core.dirman"] = {
-                    config = {
-                        workspaces = {
-                            notes = "~/notes/",
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugiun to load first out of the rest
+        config = true,   -- This automatically runs 'require("luarocks-nvim").setup()'
+    },
+    {
+        "nvim-neorg/neorg",
+        dependecies = { "luarocks.nvim" },
+        event = "VeryLazy",
+        config = function()
+            require('neorg').setup {
+                load = {
+                    ["core.defaults"] = {},
+                    ["core.concealer"] = {
+                        config = {
+                            folds = false,
+                        }
+                    },
+                    ["core.dirman"] = {
+                        config = {
+                            workspaces = {
+                                notes = "~/notes/",
+                            },
+                            default_workspace = "notes",
                         },
-                        default_workspace = "notes",
                     },
-                },
-                ["core.qol.toc"] = {
-                    config = {
-                        close_after_use = true
+                    ["core.qol.toc"] = {
+                        config = {
+                            close_after_use = true
+                        },
                     },
+                    ["core.summary"] = {},
                 },
-                ["core.summary"] = {},
-            },
-        }
-    end,
+            }
+        end,
+    }
 }
