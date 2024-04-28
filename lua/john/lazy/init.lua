69ab2a0efda562ce5a304588ec452c8c5b1e99a3
FooNameBar <itssaturdayeverday@gmail.com>
2024-04-27 19:44:35 -0700

    Changed tailwind-sorter for tailwind-tools

--- lua/john/lazy/init.lua
+++ lua/john/lazy/init.lua
@@ -9,10 +9,8 @@ return {
     },
 
     {
-        'laytan/tailwind-sorter.nvim',
-        event = "VeryLazy",
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-        build = 'cd formatter && npm i && npm run build',
-        config = true,
+        "luckasRanarison/tailwind-tools.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {}
     },
 }
