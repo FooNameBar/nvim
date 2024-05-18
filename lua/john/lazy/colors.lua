4dd09e0c43ce43ac4a3d40d8fedc11b6e5c947b4
FooNameBar <itssaturdayeverday@gmail.com>
2024-05-18 18:15:36 +0000

    Telescope: update to latest release; Neorg: removed; Colors: solid background

--- lua/john/lazy/colors.lua
+++ lua/john/lazy/colors.lua
@@ -2,8 +2,6 @@ function ColorMyPencils(color)
     color = color or "tokyonight-storm"
 
     vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 return {
@@ -13,7 +11,6 @@ return {
     config = function()
         require('tokyonight').setup({
             style = "storm",
-            transparent = true,
             terminal_colors = true,
             styles = {
                 comments = { italic = false },
