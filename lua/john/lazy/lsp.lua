133aeb3c9708e8f0e7ae03099d5365b793f5e85a
FooNameBar <itssaturdayeverday@gmail.com>
2024-06-09 22:01:00 -0700

    LSP: swapped doc scroll

--- lua/john/lazy/lsp.lua
+++ lua/john/lazy/lsp.lua
@@ -149,8 +149,8 @@ return {
                     ['<Tab'] = nil,
                     ['<S-Tab>'] = nil,
 
-                    ['<C-d>'] = cmp.mapping.scroll_docs(-2),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(2),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(2),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-2),
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
